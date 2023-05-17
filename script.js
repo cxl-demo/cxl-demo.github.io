3d5904adff768772bca65a2cd69ee7129025c81b
cxl-demo <133798783+cxl-demo@users.noreply.github.com>
2023-05-18 00:50:21 +0800

    登录功能

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ signUpBtn.addEventListener("click", () => {
 
 firstForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+
+//登录
+// 定义一个数组，用于存放用户信息
+var users = [
+  { email: '123@qq.com', password: '123456' },
+  { email: '456@qq.com', password: '654321' },
+  { email: '789@qq.com', password: 'abcdef' }
+];
+
+
+//验证email与密码是否正确
+function validate() {
+
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+  // 遍历数组中的每个用户信息，进行匹配
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].email === email && users[i].password === password) {
+      // 如果找到了匹配的用户信息，返回true
+      alert('登录成功')
+    }
+  }
+
+  // 如果循环结束后还没有找到匹配的用户信息，返回false
+  alert('输出的邮箱或密码有错误')
+}
