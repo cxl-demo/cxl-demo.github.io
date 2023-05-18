70a6513df44bf4398d389cfff08f521beacc7624
cxl-demo <133798783+cxl-demo@users.noreply.github.com>
2023-05-18 16:10:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,42 +5,99 @@ const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
 
 signInBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+  container.classList.remove("right-panel-active");
 });
 
 signUpBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
+  container.classList.add("right-panel-active");
 });
 
 firstForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
 
 
-//登录
-// 定义一个数组，用于存放用户信息
-var users = [
-  { email: '123@qq.com', password: '123456' },
-  { email: '456@qq.com', password: '654321' },
-  { email: '789@qq.com', password: 'abcdef' }
-];
 
 
-//验证email与密码是否正确
-const btn =document.getElementById("validate");
-btn.addEventListener("click", () => {
-var email = document.getElementById('email').value;
-var password = document.getElementById('password').value;
+const SignIn = document.getElementById("Signin");
+const SignUp = document.getElementById("Signup");
 
-  // 遍历数组中的每个用户信息，进行匹配
-  for (var i = 0; i < users.length; i++) {
-    if (users[i].email === email && users[i].password === password) {
-      // 如果找到了匹配的用户信息，返回true
-      alert('登录成功');
-      console.log('登陆成功')
+
+//登录功能
+SignIn.addEventListener("click", () => {
+  //获取本地users.text文件，并转化成json格式
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'users.txt');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const data = xhr.responseText;
+      const startIndex = data.indexOf('[');
+      const endIndex = data.lastIndexOf(']');
+      const usersText = data.substring(startIndex, endIndex + 1);
+      const users = JSON.parse(usersText);
+
+
+      //获取表单数据
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      // 遍历数组中的每个用户信息，进行匹配
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].email === email && users[i].password === password) {
+          // 如果找到了匹配的用户信息，返回true
+          alert(users[i].username + ',登录成功');
+          return
+        }
+      }
+      // 如果循环结束后还没有找到匹配的用户信息，返回false
+      alert('输出的邮箱或密码有错误')
     }
-  }
+  };
+  xhr.send();
 
-  // 如果循环结束后还没有找到匹配的用户信息，返回false
-  alert('输出的邮箱或密码有错误')
-  console.log('登陆失败')
+})
+
+//注册功能
+SignUp.addEventListener("click", () => {
+  //获取本地users.text文件，并转化成json格式
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'users.txt');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const data = xhr.responseText;
+      const startIndex = data.indexOf('[');
+      const endIndex = data.lastIndexOf(']');
+      const usersText = data.substring(startIndex, endIndex + 1);
+      const users = JSON.parse(usersText);
+
+
+      //获取表单数据
+      var username = document.getElementById('username').value;
+      var email = document.getElementById('Email').value;
+      var password = document.getElementById('Password').value;
+
+      for (i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+          alert("用户名：" + username + " 已存在，请重新输入")
+          return
+        }
+      }
+
+      for (i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+          alert("邮箱地址：" + email + " 已存在，请重新输入")
+          return
+        }
+      }
+
+      users.push({
+        "username": username,
+        "email": email,
+        "password": password
+      })
+
+      console.log(users);
+
+      alert("注册成功")
+    }
+  };
+  xhr.send();
 })
